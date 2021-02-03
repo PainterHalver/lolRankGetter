b1557105f50a98b601cbdae8bb4abdeb2f56245f
PainterHalver <hieptrang13042002@gmail.com>
2021-02-03 11:46:56 +0700

    added while loop and options functionality

--- __init__.py
+++ __init__.py
@@ -1,17 +1,26 @@
 from getRank import *
 
-input("Press Enter to init...")
-try:
-    lobbyInit()
-    input("Done! Press Enter when game starts to load...")
-except:
-    quit = input("Can't lobbyInit! Maybe get into a game lobby first? Press Enter to go to gameStartInit or type 'exit' to quit...")
-    if quit == "exit":
-        quit()
+print("1. In lobby")
+print("2. In game loading screen")
+print("3. Exit")
 
-try:
-    gameStartInit()
-    input("The End! Press Enter to exit...")
-except:
-    input("Problem with gameStartInit! Press Enter to exit...")
-    quit()
+while True:
+    option = int(input("Your option: "))
+    if option == 1:
+        try:
+            lobbyInit()
+            print("Done!")
+        except:
+            print("Can't lobbyInit! Maybe you're not in a game lobby?")
+            
+
+    if option == 2:
+        try:
+            gameStartInit()
+            print("Done!")
+        except:
+            print("Problem with gameStartInit! Maybe game hasn't started yet?")
+
+    if option == 3:
+        break
+quit()
